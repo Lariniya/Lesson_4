d64c4b3ff4fdd4e047bbe040bc2907d2cc0e8ac0
spolyakova <svetlana.polyakova@dataart.com>
2020-02-18 15:27:56 +0300

    add fibonacci formula

--- src/Fibonacci.java
+++ src/Fibonacci.java
@@ -1,28 +1,17 @@
 public class Fibonacci {
-    public static void main(String[] args) {
-
-
-
-        int k = 11;
+    public static void main(String[] args){
         int n0 = 1;
         int n1 = 1;
         int n2;
-        private static int calculationFibonacci (k, n0, n1){
-            System.out.print(n0 + " " + n1 + " ");
-            for (int i = 3; i <= k; i++) {
-                n2 = n0 + n1;
-                System.out.print(n2 + " ");
-                n0 = n1;
-                n1 = n2;
-            }
-            System.out.println();
-        }
+        int k = 11;
 
-        private static int calculationRecursionFibonacci ( int k){
-            if (k == 0) {
-                return 0;
-            }
-            return k + calculationRecursionFibonacci(k - 1);
+        System.out.print(n0 + " " + n1 + " ");
+        for(int i = 3; i <= k; i ++) {
+            n2 = n0 + n1;
+            System.out.print(n2 + " ");
+            n0 = n1;
+            n1 = n2;
         }
+        System.out.println();
     }
 }
